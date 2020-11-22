f646846ed18c12ddf4728d9c5593ff09bbf84c60
Natalya Berdnikova <kitakava@mail.ru>
2020-11-22 14:33:39 +0300

    Добавлены якори в навигацию, добавлены ховеры и сделаны финальные правки

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
 let sliderHome = new Swiper('.js-slider-home', {
-    loop: true,
-    pagination: {
-      el: '.slider__pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.slider__navigation-next',
-      prevEl: '.slider__navigation-prev',
-    },
+  loop: true,
+  pagination: {
+    el: '.slider__pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.slider__navigation-next',
+    prevEl: '.slider__navigation-prev',
+  },
 });
 
 let sliderPosts = new Swiper('.js-slider-posts', {
@@ -19,3 +19,15 @@ let sliderPosts = new Swiper('.js-slider-posts', {
     prevEl: '.posts__navigation-prev',
   },
 });
+
+let filterCategory = document.querySelectorAll('.filter__category')
+
+if (filterCategory !== undefined) {
+  for (let i = 0; i < filterCategory.length; i++) {
+    filterCategory[i].addEventListener('click', function () {
+      let filterCategoryActive = document.querySelector('.filter__category--active');
+      filterCategoryActive.classList.remove('filter__category--active');
+      filterCategory[i].classList.add('filter__category--active');
+    })
+  }
+}
